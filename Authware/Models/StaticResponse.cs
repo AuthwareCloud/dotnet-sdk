cf26f80ca08702d1449539e3e4465c8b23626781
Harry <59972716+dnlib@users.noreply.github.com>
2022-03-30 14:30:04 +0100

    Update generic type names

--- Authware/Models/StaticResponse.cs
+++ Authware/Models/StaticResponse.cs
@@ -3,11 +3,11 @@
 /// <summary>
 ///     A wrapper around a instanced Authware request
 /// </summary>
-/// <typeparam name="T">The type of response that is being wrapped around</typeparam>
-public class StaticResponse<T> : ErrorResponse
+/// <typeparam name="TResponse">The type of response that is being wrapped around</typeparam>
+public class StaticResponse<TResponse> : ErrorResponse
 {
     /// <summary>
     ///     The response received by the Authware API, this is the data that is returned on a successful response
     /// </summary>
-    public T Response { get; set; }
+    public TResponse Response { get; set; }
 }
